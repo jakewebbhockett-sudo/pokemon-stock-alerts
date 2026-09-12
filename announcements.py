ad9f2508a661fa2fe53170294dfab9efbd2d7d9d
jakewebbhockett-sudo <jakewebbhockett@gmail.com>
2026-09-12 22:28:32 +0100

    Create announcements.py

---
+++ announcements.py
@@ -0,0 +1,16 @@
+def get_announcements():
+    """
+    Return Pokémon restock and release announcements.
+
+    Each announcement should include:
+    - title
+    - store
+    - source
+    - url
+    - type
+    - confidence
+    """
+
+    announcements = []
+
+    return announcements
